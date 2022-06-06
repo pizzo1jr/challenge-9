73ea70f895f3798ca6f9316b9de7ae588481358a
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2022-06-06 00:08:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Video
+
+![2022-06-06_00-00-27](https://user-images.githubusercontent.com/102200085/172092587-b170710b-dfb1-43b8-b34f-e2608d449e76.gif)
+
+
+<h1 align="center">Readme Generator </h1>
+  
+![badge](https://img.shields.io/badge/license-ISC-brightgreen)<br />
+## Description
+ creates an easy readme file
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+ none
+## Usage
+ creating easy readme files
+## License
+![badge](https://img.shields.io/badge/license-ISC-brightgreen)
+<br />
+This application is covered by the ISC license. 
+## Contributing
+ Joe Pizzo
+## Tests
+ no
+## Questions
+ call<br />
+<br />
+:octocat: Find me on GitHub: [pizzo1jr](https://github.com/pizzo1jr)<br />
+<br />
+ Email me with any questions: josephpizzo@rocketmortgage.com<br /><br />
+_This README was generated with  by [README-generator](https://github.com/pizzo1jr/challenge-9)_
---
+++ develop/utils/generateReadme.js
@@ -0,0 +1,40 @@
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+    return `
+<h1 align="center">${answers.projectTitle} </h1>
+  
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+## Description
+ ${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+ ${answers.installation}
+## Usage
+ ${answers.usage}
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+## Contributing
+ ${answers.contributing}
+## Tests
+ ${answers.tests}
+## Questions
+ ${answers.questions}<br />
+<br />
+:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+ Email me with any questions: ${answers.email}<br /><br />
+_This README was generated with  by [README-generator](https://github.com/pizzo1jr/challenge-9)_
+    `;
+  }
+  
+  module.exports = generateReadme;
+  
